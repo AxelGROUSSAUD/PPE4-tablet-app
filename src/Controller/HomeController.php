22954d7fa6da1ef0fe91f4fab52bf19a92e764e3
HUGO <hugofeige@hotmail.fr>
2021-05-03 17:05:44 +0200

    display publish form

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\PublishType;
 
 class HomeController extends AbstractController
 {
--- src/Form/PublishType.php
+++ src/Form/PublishType.php
@@ -5,6 +5,7 @@ namespace App\Form;
 use App\Entity\Avis;
 use App\Entity\PhotoClient;
 use Symfony\Component\Form\Extension\Core\Type\FileType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -14,8 +15,8 @@ class PublishType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('file', FileType::class)
-            ->add('commentaire',TextType::class,['label'=> 'Votre commentaire']);
+            ->add('file', FileType::class, ['label' => 'Votre image : '])
+            ->add('commentaire',TextType::class,['label'=> 'Votre commentaire : ']);
         ;
     }
 
